e2e07a925b462a71f568771611fd3c99d73f8abd
Luis Serpas <serpas.icode@gmail.com>
2020-03-19 18:47:06 -0400

    Initial Spring Boot Application

---
+++ src/main/java/com/algorithm_factory/TheAlgorithmFactoryApplication.java
@@ -0,0 +1,13 @@
+package com.algorithm_factory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheAlgorithmFactoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheAlgorithmFactoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/algorithm_factory/TheAlgorithmFactoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.algorithm_factory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheAlgorithmFactoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
