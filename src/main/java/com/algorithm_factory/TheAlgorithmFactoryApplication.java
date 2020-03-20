a6669003fb7f4fed8e914c0528e00b6a6a961bf0
Luis Serpas <serpas.icode@gmail.com>
2020-03-20 19:18:01 -0400

    Add Bootstrap 4 Navbar, link to library and add Basic CSS

--- src/main/java/com/algorithm_factory/TheAlgorithmFactoryApplication.java
+++ src/main/java/com/algorithm_factory/TheAlgorithmFactoryApplication.java
@@ -1,4 +1,4 @@
-package com.algorithm_factory;
+ package com.algorithm_factory;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
