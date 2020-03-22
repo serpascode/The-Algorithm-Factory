0bcc5c10f5169dc7ec74d0ea58923d8a03f68d4b
Luis Serpas <serpas.icode@gmail.com>
2020-03-22 19:15:29 -0400

    Update CSS, && Begin Animating Algorithm with Javascript

--- src/main/java/com/algorithm_factory/HomeController.java
+++ src/main/java/com/algorithm_factory/HomeController.java
@@ -17,24 +17,28 @@ public class HomeController {
 	@RequestMapping("/insertionSort")
 	public String insertionSortView(ModelMap model) {
 		model.addAttribute("content", "insertionSort");
+		model.addAttribute("displayInsertionSort", true);
 		return "index";
 	}
 	
 	@RequestMapping("/bubbleSort")
 	public String bubbleSortView(ModelMap model) {
 		model.addAttribute("content", "bubbleSort");
+		model.addAttribute("displayBubbleSort", true);
 		return "index";
 	}
 	
 	@RequestMapping("/quickSort")
 	public String quickSortView(ModelMap model) {
 		model.addAttribute("content", "quickSort");
+		model.addAttribute("displayQuickSort", true);
 		return "index";
 	}
 	
 	@RequestMapping("/mergeSort")
 	public String mergeSortView(ModelMap model) {
 		model.addAttribute("content", "mergeSort");
+		model.addAttribute("displayMergeSort", true);
 		return "index";
 	}
 }
