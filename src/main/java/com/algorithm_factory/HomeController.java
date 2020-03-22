47e65d362786e10404a45feec2035ebbccc0df54
Luis Serpas <serpas.icode@gmail.com>
2020-03-22 13:26:59 -0400

    Refactor index page to contain variable views for each of the Algorithms, Added Controller Mapping

--- src/main/java/com/algorithm_factory/HomeController.java
+++ src/main/java/com/algorithm_factory/HomeController.java
@@ -1,14 +1,40 @@
 package com.algorithm_factory;
 
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
 	
 	@RequestMapping("/")
-	public String index() {
+	public String index(ModelMap model) {
+		model.addAttribute("content", "welcome");
 		return "index";
 	}
-
+	
+	@RequestMapping("/insertionSort")
+	public String insertionSortView(ModelMap model) {
+		model.addAttribute("content", "insertionSort");
+		return "index";
+	}
+	
+	@RequestMapping("/bubbleSort")
+	public String bubbleSortView(ModelMap model) {
+		model.addAttribute("content", "bubbleSort");
+		return "index";
+	}
+	
+	@RequestMapping("/quickSort")
+	public String quickSortView(ModelMap model) {
+		model.addAttribute("content", "quickSort");
+		return "index";
+	}
+	
+	@RequestMapping("/mergeSort")
+	public String mergeSortView(ModelMap model) {
+		model.addAttribute("content", "mergeSort");
+		return "index";
+	}
 }
